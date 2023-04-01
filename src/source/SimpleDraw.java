e8e99869a48a18eded7f21e175e1216ce13cfd9f
e99999 <129567965+stuffbymack@users.noreply.github.com>
2023-03-31 21:27:00 -0700

    much much better working prototype

--- src/source/SimpleDraw.java
+++ src/source/SimpleDraw.java
@@ -1,124 +1,93 @@
 package source;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.RenderedImage;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class SimpleDraw extends JFrame implements MouseListener, MouseMotionListener, ActionListener {
+public class SimpleDraw extends JFrame implements MouseMotionListener, ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel canvas, buttonPanel;
-    private int brushSize = 15;
-    private int lastX, lastY;
-    private Image bufferImage;
-    private Graphics bufferGraphics;
-    private JButton clearButton, saveButton;
-    private JFileChooser fileChooser;
-    private FileNameExtensionFilter pngFilter;
-
+	private BufferedImage canvas;
+    private Graphics2D g2d;
+    private JButton saveButton;
+    private JButton clearButton;
+    private Point mousePosition;
+    
     public SimpleDraw() {
-    	setTitle("SimpleDraw");
-        setSize(512, 512);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBackground(Color.WHITE);
-
-        // Panel for drawing canvas
-        canvas = new JPanel() {
-			private static final long serialVersionUID = 2L;
-			public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                if (bufferImage != null) {
-                    g.drawImage(bufferImage, 0, 0, null);
-                }
-            }
-        };
-        canvas.addMouseListener(this);
-        canvas.addMouseMotionListener(this);
-        add(canvas);
-
-        // Panel just for buttons
-        buttonPanel = new JPanel(new BorderLayout());
+        super("SimpleDraw");
+        setResizable(false);
+        setLocation(700, 250);
         
-        // Clear button created for erasing the canvas
-        clearButton = new JButton("Clear");
-        clearButton.addActionListener(e -> clearCanvas());
-        buttonPanel.add(clearButton, BorderLayout.NORTH);
-
-        // Save button added for exporting the canvas
+        canvas = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
+        g2d = canvas.createGraphics();
+        g2d.setColor(Color.BLACK);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setStroke(new BasicStroke(15, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        
+        // Set the canvas background to white
+        g2d.setPaint(Color.WHITE);
+        g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        g2d.setPaint(Color.BLACK);
+        
+        JPanel buttonPanel = new JPanel(new FlowLayout());
         saveButton = new JButton("Save");
-        saveButton.addActionListener(e -> saveCanvas());
-        buttonPanel.add(saveButton, BorderLayout.SOUTH);
-
-        add(buttonPanel, BorderLayout.NORTH);
-
-        fileChooser = new JFileChooser();
-        pngFilter = new FileNameExtensionFilter("PNG files", "png");
-        fileChooser.setFileFilter(pngFilter);
-
+        clearButton = new JButton("Clear");
+        buttonPanel.add(saveButton);
+        buttonPanel.add(clearButton);
+        
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(new JLabel(new ImageIcon(canvas)), BorderLayout.CENTER);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        
+        saveButton.addActionListener(this);
+        clearButton.addActionListener(this);
+        addMouseMotionListener(this);
+        
+        setSize(512, 564);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-
-        bufferImage = createImage(getWidth(), getHeight());
-        bufferGraphics = bufferImage.getGraphics();
-        bufferGraphics.setColor(Color.WHITE);
-        bufferGraphics.fillRect(0, 0, getWidth(), getHeight());
     }
-
-    private void clearCanvas() {
-        bufferGraphics.setColor(Color.WHITE);
-        bufferGraphics.fillRect(0, 0, getWidth(), getHeight());
-        canvas.getGraphics().drawImage(bufferImage, 0, 0, null);
-    }
-
-    private void saveCanvas() {
-        int result = fileChooser.showSaveDialog(this);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            String filePath = fileChooser.getSelectedFile().getPath();
-            if (!filePath.endsWith(".png")) {
-                filePath += ".png";
-            }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == saveButton) {
             try {
-                ImageIO.write((RenderedImage) bufferImage, "png", new File(filePath));
-            } catch (IOException e) {
-                e.printStackTrace();
+                JFileChooser chooser = new JFileChooser();
+                chooser.setCurrentDirectory(chooser.getFileSystemView().getDefaultDirectory());
+                int result = chooser.showSaveDialog(this);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                	String filePath = chooser.getSelectedFile().getPath();
+                    if (!filePath.endsWith(".png")) {
+                        filePath += ".png";
+                    }
+                    ImageIO.write(canvas, "png", new File(filePath));
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
+        } else if (e.getSource() == clearButton) {
+            g2d.setPaint(Color.WHITE);
+            g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            g2d.setPaint(Color.BLACK);
+            getContentPane().repaint();
         }
     }
     
-    public void mousePressed(MouseEvent e) {
-        lastX = e.getX();
-        lastY = e.getY();
-        drawBrush(lastX, lastY, e.getX(), e.getY());
+    public void mouseDragged(MouseEvent e) {
+        if (mousePosition != null) {
+            int x = e.getX();
+            int y = e.getY();
+            g2d.drawLine(mousePosition.x, mousePosition.y - 16, x, y -16);
+            mousePosition = new Point(x, y);
+            getContentPane().repaint();
+        }
     }
 
-    public void mouseDragged(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-        drawBrush(lastX, lastY, x, y);
-        lastX = x;
-        lastY = y;
+    public void mouseMoved(MouseEvent e) {
+        mousePosition = new Point(e.getX(), e.getY());
     }
-    
-    private void drawBrush(int x1, int y1, int x2, int y2) {
-        bufferGraphics.setColor(Color.BLACK);
-        bufferGraphics.fillOval(x2 - brushSize / 2, y2 - brushSize / 2, brushSize, brushSize);
-        canvas.getGraphics().drawImage(bufferImage, 0, 0, null);
-    }
-    
-    // Useless methods needed for implementation.
-    public void actionPerformed(ActionEvent e) {}
-    public void mouseMoved(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mouseClicked(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseDragExited(MouseEvent e) {}
-    public void mouseDragEntered(MouseEvent e) {}
 
     public static void main(String[] args) {
         new SimpleDraw();
