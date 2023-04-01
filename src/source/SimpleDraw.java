7643b6c62935419ffaa94aa592a25dfc4649af96
e99999 <129567965+stuffbymack@users.noreply.github.com>
2023-04-01 10:08:34 -0700

    format and cleanup

--- src/source/SimpleDraw.java
+++ src/source/SimpleDraw.java
@@ -11,108 +11,123 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, ActionLis
 
 	private static final long serialVersionUID = 1L;
 	private BufferedImage canvas;
-    private Graphics2D g2d;
-    private JButton saveButton;
-    private JButton clearButton;
-    private JButton colorButton;
-    private Point mousePosition;
-    
-    public SimpleDraw() {
-        super("SimpleDraw");
-        setResizable(false);
-        setLocation(700, 250);
-        try {
+	private Graphics2D g2d;
+	private JButton saveButton;
+	private JButton clearButton;
+	private JButton colorButton;
+	private Point mousePosition;
+	private static final String NAME = "SimpleDraw";
+	private static final String SAVE = "Save";
+	private static final String CLEAR = "Clear";
+	private static final String COLOR = "Color";
+
+	public SimpleDraw() {
+		super(NAME);
+		// Not resize-able, will break drawing if resized at the moment.
+		setResizable(false);
+		// Attempt to mimic the system look and feel
+		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        canvas = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
-        g2d = canvas.createGraphics();
-        g2d.setColor(Color.BLACK);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setStroke(new BasicStroke(15, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-        
-        // Set the canvas background to white
-        g2d.setPaint(Color.WHITE);
-        g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        g2d.setPaint(Color.BLACK);
-        
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        saveButton = new JButton("Save");
-        clearButton = new JButton("Clear");
-        colorButton = new JButton("Color");
-        buttonPanel.add(saveButton);
-        buttonPanel.add(clearButton);
-        buttonPanel.add(colorButton);
-        
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(new JLabel(new ImageIcon(canvas)), BorderLayout.CENTER);
-        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-        
-        saveButton.addActionListener(this);
-        clearButton.addActionListener(this);
-        colorButton.addActionListener(this);
-        addMouseMotionListener(this);
-        
-        setSize(512, 564);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == saveButton) {
-            try {
-                JFileChooser chooser = new JFileChooser();
-                chooser.setCurrentDirectory(chooser.getFileSystemView().getDefaultDirectory());
-                int result = chooser.showSaveDialog(this);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                	String filePath = chooser.getSelectedFile().getPath();
-                    if (!filePath.endsWith(".png")) {
-                        filePath += ".png";
-                    }
-                    ImageIO.write(canvas, "png", new File(filePath));
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } else if (e.getSource() == clearButton) {
-            g2d.setPaint(Color.WHITE);
-            g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-            g2d.setPaint(Color.BLACK);
-            getContentPane().repaint();
-        } else if (e.getSource() == colorButton) {
-        	Color initialcolor= g2d.getColor();    
-        	Color color= JColorChooser.showDialog(this,"Select a color",initialcolor);    
-        	g2d.setPaint(color);    
-        }  
-    }
-    
-    public void mouseDragged(MouseEvent e) {
-    	if (mousePosition != null) {
-            int x = e.getX();
-            int y = e.getY();
-            g2d.drawLine(mousePosition.x, mousePosition.y - 16, x, y -16);
-            mousePosition = new Point(x, y);
-            getContentPane().repaint();
-        }
-    }
+		// Build 2d canvas object in JFrame
+		canvas = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
+		g2d = canvas.createGraphics();
+		g2d.setColor(Color.BLACK);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setStroke(new BasicStroke(15, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+		// Set the canvas background to white
+		g2d.setPaint(Color.WHITE);
+		g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		g2d.setPaint(Color.BLACK);
+		// Button factory
+		JPanel buttonPanel = new JPanel(new FlowLayout());
+		saveButton = new JButton(SAVE);
+		clearButton = new JButton(CLEAR);
+		colorButton = new JButton(COLOR);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(colorButton);
+		buttonPanel.add(clearButton);
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(new JLabel(new ImageIcon(canvas)), BorderLayout.CENTER);
+		getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+		saveButton.addActionListener(this);
+		clearButton.addActionListener(this);
+		colorButton.addActionListener(this);
+		// JFrame parameters
+		addMouseMotionListener(this);
+		setSize(512, 564);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Sets the window to be center screen
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 
-    public void mouseMoved(MouseEvent e) {
-        mousePosition = new Point(e.getX(), e.getY());
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == saveButton) {
+			saveCanvas();
+		} else if (e.getSource() == clearButton) {
+			clearCanvas();
+		} else if (e.getSource() == colorButton) {
+			chooseColor();
+		}
+	}
 
-    public static void main(String[] args) {
-        new SimpleDraw();
-    }
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (mousePosition != null) {
+			int x = e.getX();
+			int y = e.getY();
+			// TODO Fix this bad fix for brush/mouse offset
+			// the "- 16" fixes some inherent offset from the JFrame boiler itself, will break if window is resized.
+			g2d.drawLine(mousePosition.x, mousePosition.y - 16, x, y - 16);
+			mousePosition = new Point(x, y);
+			getContentPane().repaint();
+		}
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		mousePosition = new Point(e.getX(), e.getY());
+	}
+	
+
+	public void saveCanvas () {
+		try {
+			JFileChooser chooser = new JFileChooser();
+			chooser.setCurrentDirectory(chooser.getFileSystemView().getDefaultDirectory());
+			int result = chooser.showSaveDialog(this);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				String filePath = chooser.getSelectedFile().getPath();
+				if (!filePath.endsWith(".png")) {
+					filePath += ".png";
+				}
+				ImageIO.write(canvas, "png", new File(filePath));
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	public void clearCanvas() {
+		g2d.setPaint(Color.WHITE);
+		g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		g2d.setPaint(Color.BLACK);
+		getContentPane().repaint();
+	}
+	
+	public void chooseColor() {
+		Color initialcolor = g2d.getColor();    
+    	Color color = JColorChooser.showDialog(this,COLOR,initialcolor);
+    	g2d.setPaint(color);  
+	}
 }
