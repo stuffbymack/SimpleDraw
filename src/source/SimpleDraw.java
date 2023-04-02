f45b53b72f311821b1690f60f8e74d7796116e80
e99999 <129567965+stuffbymack@users.noreply.github.com>
2023-04-01 17:33:07 -0700

    removed useless code, cleaned up massive constructor

--- src/source/SimpleDraw.java
+++ src/source/SimpleDraw.java
@@ -6,6 +6,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
@@ -13,7 +16,7 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, MouseList
 
 	private static final long serialVersionUID = 1L;
 	private BufferedImage canvas;
-	private Graphics2D g2d;
+	private Graphics2D graphics;
 	private JButton saveButton;
 	private JButton clearButton;
 	private JButton colorButton;
@@ -21,70 +24,104 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, MouseList
 	private JDialog colorDialog;
 	private Point mousePosition;
 	private boolean mousePressed;
-	private static final String NAME = "SimpleDraw";
-	private static final String SAVE = "Save";
-	private static final String CLEAR = "Clear";
-	private static final String PALETTE = "Palette";
+	private JPanel buttonPanel;
+	private static final String FRAME_NAME = "SimpleDraw";
+	private static final String SAVE_BUTTON = "Save";
+	private static final String CLEAR_BUTTON = "Clear";
+	private static final String PALETTE_BUTTON = "Palette";
+	private static final int CANVAS_SIZE = 512;
+	private static final int BRUSH_SIZE = 15;
 
 	public SimpleDraw() {
-		super(NAME);
+		super(FRAME_NAME);
+		setupColorChooser();
+		setupColorDialog();
+		setupResizable();
+		setupLookAndFeel();
+		setupCanvas();
+		setupGraphics();
+		setupButtons();
+		setupContentPane();
+		setupListeners();
+		setupJFrame();
+	}
+
+	private void setupColorChooser() {
 		colorChooser = new MinimalColorChooser(Color.BLACK);
-		colorChooser.setPreviewPanel(new JPanel()); // disable the preview panel
+		colorChooser.setPreviewPanel(new JPanel());
+	}
+
+	private void setupColorDialog() {
 		colorDialog = new JDialog();
 		colorDialog.add(colorChooser);
 		colorDialog.pack();
 		colorDialog.setResizable(false);
-		colorDialog.setTitle(PALETTE);
-		// Not resize-able, will break drawing if resized at the moment.
+		colorDialog.setTitle(PALETTE_BUTTON);
+	}
+
+	private void setupResizable() {
 		setResizable(false);
-		// Attempt to mimic the system look and feel
+	}
+
+	private void setupLookAndFeel() {
 		updateLookAndFeel();
-		// Build 2d canvas object in JFrame
-		canvas = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
-		g2d = canvas.createGraphics();
-		g2d.setColor(Color.BLACK);
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.setStroke(new BasicStroke(15, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-		// Set the canvas background to white
-		g2d.setPaint(Color.WHITE);
-		g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		g2d.setPaint(Color.BLACK);
-		// Button factory
-		JPanel buttonPanel = new JPanel(new FlowLayout());
-		saveButton = new JButton(SAVE);
-		clearButton = new JButton(CLEAR);
-		colorButton = new JButton(PALETTE);
+	}
+
+	private void setupCanvas() {
+		canvas = new BufferedImage(CANVAS_SIZE, CANVAS_SIZE, BufferedImage.TYPE_INT_ARGB);
+	}
+
+	private void setupGraphics() {
+		graphics = canvas.createGraphics();
+		graphics.setColor(Color.BLACK);
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.setStroke(new BasicStroke(BRUSH_SIZE, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+		graphics.setPaint(Color.WHITE);
+		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	}
+
+	private void setupButtons() {
+		saveButton = new JButton(SAVE_BUTTON);
+		clearButton = new JButton(CLEAR_BUTTON);
+		colorButton = new JButton(PALETTE_BUTTON);
+		buttonPanel = new JPanel(new FlowLayout());
 		buttonPanel.add(saveButton);
 		buttonPanel.add(colorButton);
 		buttonPanel.add(clearButton);
+		saveButton.addActionListener(this);
+		colorButton.addActionListener(this);
+		clearButton.addMouseListener(new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+				if (SwingUtilities.isLeftMouseButton(e)) {
+					clearCanvas();
+				} else if (SwingUtilities.isRightMouseButton(e)) {
+					colorCanvas();
+				}
+			}
+		});
+	}
+
+	private void setupContentPane() {
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(new JLabel(new ImageIcon(canvas)), BorderLayout.CENTER);
 		getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-		saveButton.addActionListener(this);
-		//Extra clear button magic for right click fill with color behavior
-		clearButton.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                if (SwingUtilities.isLeftMouseButton(e)) {
-                    clearCanvas();
-                } else if (SwingUtilities.isRightMouseButton(e)) {
-                	colorCanvas();
-                }
-            }
-        });
-		colorButton.addActionListener(this);
-		// JFrame parameters
+	}
+
+	private void setupListeners() {
 		addMouseMotionListener(this);
 		addMouseListener(this);
+	}
+
+	private void setupJFrame() {
 		setSize(524, 584);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Sets the window to be center screen
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//The clear button is not handled here because it has added functionality for left/right click
 		if (e.getSource() == saveButton) {
 			saveCanvas();
 		} else if (e.getSource() == colorButton) {
@@ -93,41 +130,38 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, MouseList
 	}
 
 	@Override
+	public void mousePressed(MouseEvent e) {
+		mousePressed = true;
+		graphics.setPaint(colorChooser.getColor());
+		graphics.fillOval(mousePosition.x - 13, mousePosition.y - 36, BRUSH_SIZE, BRUSH_SIZE);
+		getContentPane().repaint();
+	}
+
+	@Override
 	public void mouseDragged(MouseEvent e) {
 		if (mousePosition != null && mousePressed) {
-			// This can be better, it gets the color every drag event
-			g2d.setPaint(colorChooser.getColor());
+			graphics.setPaint(colorChooser.getColor());
 			int x = e.getX();
 			int y = e.getY();
 			// TODO Fix this bad fix for brush/mouse offset
 			// 29 = (584 - 512) / 2 - (Brush / 2)
 			// 6 = ?
-			g2d.drawLine(mousePosition.x - 6, mousePosition.y - 29, x - 6, y - 29);
+			graphics.drawLine(mousePosition.x - 6, mousePosition.y - 29, x - 6, y - 29);
 			mousePosition = new Point(x, y);
 			getContentPane().repaint();
 		}
-	}
-
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-		mousePressed = true;
-		// Draws a single circle when clicked for full action of mouse
-		g2d.setPaint(colorChooser.getColor());
-		g2d.fillOval(mousePosition.x - 13, mousePosition.y - 36, 15, 15);
-		getContentPane().repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		mousePressed = false;
 	}
-	
+
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		mousePosition = new Point(e.getX(), e.getY());
 	}
-	
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
@@ -142,35 +176,41 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, MouseList
 
 	private String lastSavedFilePath = System.getProperty("user.dir");
 
-    public void saveCanvas() {
-        try {
-            JFileChooser chooser = new JFileChooser();
-            chooser.setCurrentDirectory(new File(lastSavedFilePath));
-            int result = chooser.showSaveDialog(this);
-            if (result == JFileChooser.APPROVE_OPTION) {
-                String filePath = chooser.getSelectedFile().getPath();
-                if (!filePath.endsWith(".png")) {
-                    filePath += ".png";
-                }
-                ImageIO.write(canvas, "png", new File(filePath));
-                lastSavedFilePath = filePath;
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	public void saveCanvas() {
+		try {
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory(new File(lastSavedFilePath));
+			int result = fileChooser.showSaveDialog(this);
+			if (result == JFileChooser.APPROVE_OPTION) {
+				String selectedFilePath = fileChooser.getSelectedFile().getPath();
+				if (!selectedFilePath.endsWith(".png")) {
+					selectedFilePath += ".png";
+				}
+				try (FileOutputStream fos = new FileOutputStream(selectedFilePath)) {
+					ImageIO.write(canvas, "png", fos);
+					lastSavedFilePath = selectedFilePath;
+				} catch (FileNotFoundException ex) {
+					System.err.println("Error: Unable to save file - File not found.");
+				} catch (IOException ex) {
+					System.err.println("Error: Unable to save file - IO Exception.");
+				} catch (Exception ex) {
+					System.err.println("Error: Unable to save file.");
+				}
+			}
+		} catch (Exception ex) {
+			System.err.println("Error: Unable to open file chooser.");
+		}
+	}
 
 	public void clearCanvas() {
-		g2d.setPaint(Color.WHITE);
-		g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		g2d.setPaint(Color.BLACK);
+		graphics.setPaint(Color.WHITE);
+		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		getContentPane().repaint();
 	}
 
 	public void colorCanvas() {
-		g2d.setPaint(colorChooser.getColor());
-		g2d.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		g2d.setPaint(Color.BLACK);
+		graphics.setPaint(colorChooser.getColor());
+		graphics.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 		getContentPane().repaint();
 	}
 
@@ -200,5 +240,4 @@ public class SimpleDraw extends JFrame implements MouseMotionListener, MouseList
 			e.printStackTrace();
 		}
 	}
-
 }
